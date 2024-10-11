fa2ed09088c9e2575e817fffab1c85ece6e2da6a
andriylazor7 <236327@student.uek.krakow.pl>
2024-10-11 13:24:49 +0200

    Section 3 Completed

---
+++ 02-TypesAndVariables/tasks.py
@@ -0,0 +1,157 @@
+# ###
+# # A program that calculates the sum of two numbers.
+# # Modify the program to calculate the sum of three numbers.
+# #
+# number1 = 71
+# number2 = 14
+# number3 = 3
+# result = number1 + number2 + number3
+# print('Number 1: ', number1)
+# print('Number 2: ', number2)
+# print('Number 3: ', number3)
+# print('The result of summation: ', result)
+
+
+# ###
+# # A program for swapping two varable values
+# #
+# x = 7
+# y = 34
+# z = 0 # additional, auxiliary variable
+# print("Before swapping: x=", x, "y=", y)
+
+# # swap the values
+# z=x
+# x=y
+# y=z
+
+
+# print("After swapping: x=", x, "y=", y)
+
+
+
+
+# ###
+# # A program that, for a given speed in km/h,
+# # prints the speed in m/s
+# #
+# speed_kmh = 70
+# speed_ms = speed_kmh * 0.621371
+# print(speed_kmh, "km/h = ", speed_ms, "m/s")
+
+
+
+# ###
+# # A program that calculates the length of the diagonal
+# # of a rectangle with sides a and b.
+# #
+# import math
+# a = 5
+# b = 3
+# diagonal = math.sqrt(a**2 + b**2)
+# print(diagonal)
+
+
+
+# # d = 3.57 × √h
+
+import math
+# height = 20
+# result = 3.57 * math.sqrt(height / 1000)
+# print(result)
+
+# ----i
+# height1 = 1.76
+# result1 = 3.57 * math.sqrt(height1 / 1000)
+
+# print(result1)
+
+# height2 = 1.76 + 20
+# result2 = 3.57 * math.sqrt(height2 / 1000)
+
+# print(result2)
+
+
+
+
+# ###
+# # A program that calculates and prints:
+# # - the number of people and percentage of the total
+# #   population living in the Northern Hemisphere
+# # - the number of people and percentage of the total
+# #   population living in the Southern Hemisphere
+# #
+# total = 8000000000
+# north = 7200000000
+# south = total - north
+# print("World population: ", total)
+# print("Northern Hemisphere population: ", north)
+# print("Northern Hemisphere in %: ", (north / total) * 100)
+# print("Southern Hemisphere population: ", south)
+# print("Southern Hemisphere in %: ", (south / total) * 100)
+
+
+
+
+# ###
+# # A program that calculates and prints
+# # the average grade of a student
+# #
+# Math = 5
+# art = 4
+# music = 5
+# history = 3
+# average = (Math + art + music + history) / 4
+# print("Average grade is", average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
